7642c0d0018247143bac24ce44760bd005d66719
asaf arviv <asaf.arviv@gmail.com>
2023-12-20 15:24:58 +0200

    first commit - asaf - creation of Read4u project

---
+++ src/main/java/com/asystems/read4u/Read4UApplication.java
@@ -0,0 +1,13 @@
+package com.asystems.read4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Read4UApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Read4UApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asystems/read4u/Read4UApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asystems.read4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Read4UApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
